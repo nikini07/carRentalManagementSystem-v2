1a7d370a1476641d26add71b5218e278ca3b3e74
N1338382 <nikiniwarnakula@gmail.com>
2025-09-20 14:41:25 +0100

    Update tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -6,7 +6,15 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'slate-850': '#1E293B', // Custom shade for alternating table rows
+        'lime': '#A3E635',
+        'lime-dark': '#84CC16',
+        'neutral-white': '#F5F5F5',
+        'neutral-gray-light': '#E5E7EB',
+        'neutral-gray-medium': '#6B7280',
+        'neutral-gray-600': '#4B5563',
+        'neutral-gray-dark': '#374151',
+        'red-accent': '#EF4444',
+        'red-accent-600': '#DC2626',
       },
     },
   },
