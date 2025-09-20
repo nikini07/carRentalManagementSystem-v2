ea30b4d3eeac1f92551647dce3fd7ba0383276f3
N1338382 <nikiniwarnakula@gmail.com>
2025-09-20 14:04:38 +0100

    Update tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,11 +1,14 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'gray-850': '#1F2A44', // Custom shade for alternating table rows
+      },
+    },
   },
   plugins: [],
 }
